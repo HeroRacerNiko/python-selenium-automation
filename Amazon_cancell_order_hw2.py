75467444624e800336bd603da35d5a611d9afe3b
Nurdeen Nurmanbetov <nurdin.nurmanbetov@gmail.com>
2021-02-01 00:40:08 -0500

    Clearifications from Lana added

--- Amazon_cancell_order_hw2.py
+++ Amazon_cancell_order_hw2.py
@@ -10,16 +10,20 @@ driver.implicitly_wait(4)
 # help_search_x = "//input[@id='helpsearch']"
 help_search = driver.find_element(By.ID, "helpsearch")
 help_search.click()
+# help_search.send_keys("Cancel order", Keys.ENTER)
 help_search.send_keys("Cancel order")
 help_search.send_keys(Keys.ENTER)
 
 
 hcs_field = driver.find_element(By.CSS_SELECTOR, ".help-content")
 
-
-actual_text = driver.find_element(By.CSS_SELECTOR, ".help-content").text
 expected_text = 'Cancel Items or Orders'
-assert expected_text in actual_text, f'Expected {expected_text}, but got {actual_text}'
-# assert expected_text == actual_text, f'Expected {expected_text}, but got {actual_text}'
+
+# actual_text = driver.find_element(By.CSS_SELECTOR, ".help-content").text
+# assert expected_text in actual_text, f'Expected {expected_text}, but got {actual_text}'
+
+actual_text = driver.find_element(By.CSS_SELECTOR, ".help-content h1").text
+
+assert expected_text == actual_text, f'Expected {expected_text}, but got {actual_text}'
 
 driver.quit()
