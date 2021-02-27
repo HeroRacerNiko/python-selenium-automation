a1b75adc3f09714930114d1642baca469df59407
Nurdeen Nurmanbetov <nurdin.nurmanbetov@gmail.com>
2021-02-27 11:18:15 -0500

    Practice of lesson material

--- features/steps/sign_in_popup.py
+++ features/steps/sign_in_popup.py
@@ -1,8 +1,10 @@
 from behave import when, then, given
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 
 SIGN_IN_POPUP = (By.CSS_SELECTOR, "#nav-signin-tooltip .nav-action-inner")
+MAIN_SEARCH = (By.ID, 'twotabsearchtextbox')
 
 
 @when('Click on Sign In from popup')
@@ -22,3 +24,25 @@ def verify_sign_in_opened(context):
     assert "https://www.amazon.com/ap/signin" in context.driver.current_url, \
         f"'Sign in' keyword is not present in the current URL"
     assert applicant_email_form, f"email form is not present"
+# ############################ScenarioOutlineWithWait##############################################
+
+
+@when('Verify tooltip popup appears and clickable')
+def check_for_tooltip(context):
+    # either one of following verification should work
+    context.driver.wait.until(EC.element_to_be_clickable(SIGN_IN_POPUP))
+    assert context.driver.find_element(*SIGN_IN_POPUP), f"Tooltip did not load..."
+
+
+@when('Wait for {sec} seconds')
+def hard_sleep(context, sec):
+    sleep(int(sec))
+
+
+@then('Verify tooltip disappears in {sec} seconds')
+def verify_tooltip_hidden(context, sec):
+    # tooltip doesn't disappear after 4 seconds but implicit wait
+    context.driver.wait.until(EC.invisibility_of_element_located(SIGN_IN_POPUP))
+    context.driver.wait.until(EC.presence_of_element_located(MAIN_SEARCH))
+    # context.driver.find_element(*MAIN_SEARCH) # does same thing as above
+
--- features/tests/sign_in_popup.feature
+++ features/tests/sign_in_popup.feature
@@ -7,4 +7,14 @@ Feature: Amazon Sign in tests
     When Click on Sign In from popup
     Then Verify Sign In page opens
 
-
+  Scenario Outline: Sign in tooltip popup stays up for certain amount of time
+    Given Open Amazon page
+    When Verify tooltip popup appears and clickable
+    When Wait for <sec> seconds
+    When Verify tooltip popup appears and clickable
+    Then Verify tooltip disappears in <sec> seconds
+  Examples:
+    |sec|
+    |2  |
+    #|6  |
+    #|10 |
